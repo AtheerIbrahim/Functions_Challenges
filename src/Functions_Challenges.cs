7efb583f09a0c36d864ee26dcdbd33ca48199d04
AtheerIbrahim <159605191+AtheerIbrahim@users.noreply.github.com>
2024-09-16 15:30:28 +0300

    Challenge 1 added

---
+++ src/Functions_Challenges.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Functions_Challenges
+{
+    public class Program
+    {
+        public static string StringNumberProcessor(string text1, int num1, int num2, string text2)
+        {
+            int result = num1+num2;
+            return text1 + " " + text2 + ";" + " " + result;
+        }
+        public static void Main(string[] args)
+        {
+            // Challenge 1: String and Number Processor
+            Console.WriteLine("Challenge 1: String and Number Processor");
+            Console.WriteLine(StringNumberProcessor("Hello", 100, 200, "World"));
+        }
+    }
+}
